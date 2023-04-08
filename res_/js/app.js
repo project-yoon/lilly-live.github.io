d5c6529c33c95b2ae58876f132f8979753cf34cb
project-yoon <pj.yyxxmm@gmail.com>
2023-04-08 17:25:49 +0900

    copy cvnt

---
+++ res_/js/app.js
@@ -0,0 +1,1 @@
+$("#cpcv-navs").load("../app/set_/cpcv-categories.html");$("#cpcv-covers").load("./../app/set_/cpcv-front.html");function CALL(_url){$.ajax({type:'get',url:_url,success:function(data){$('#cpcv-pagecall').html(data);},error:function(){$('#cpcv-pagecall').text('waiting to write!');}});}
